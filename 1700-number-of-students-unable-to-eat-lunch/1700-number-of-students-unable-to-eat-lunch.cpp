e1805d5d57827d66f24b0ee80807850c79e89545
Rishabh <149367535+rishabh-ydv23@users.noreply.github.com>
2026-02-06 13:08:01 +0530

    Time: 0 ms (100%), Space: 11.7 MB (88.79%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
-    //this is simulation method
     int countStudents(vector<int>& students, vector<int>& sandwiches) {
-        //sandwich - stack
-        //students - queue
-        queue<int>q;
-        for(int e:students){
-            q.push(e);
-        }
+        int n=students.size();
+        int count_0=0;
+        int count_1=0;
 
-        int i=0;        //sandwich index
-        int failed=0;   //count the failed attempts
-
-        while(!q.empty()){
-            if(q.front()==sandwiches[i]){
-                q.pop();
-                i++;
-                failed=0;
+        for(int i=0;i<n;i++){
+            if(students[i]==0){
+                count_0++;
             }else{
-                q.push(q.front());
-                q.pop();
-                failed++;
-            }
-            if(failed==q.size()){
-                break;
+                count_1++;
             }
         }
 
-        return q.size();
+        for(int i=0;i<sandwiches.size();i++){
+            if(sandwiches[i]==0){
+                if(count_0==0){
+                    return sandwiches.size()-i;
+                }
+                count_0--;
+            }else if(sandwiches[i]==1){
+                if(count_1==0){
+                    return sandwiches.size()-i;
+                }
+                count_1--;
+            }
+        }
+        return 0;
     }
 };
