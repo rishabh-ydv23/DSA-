14e38bf0757793f36981434172856b39d90b1ae0
Rishabh <149367535+rishabh-ydv23@users.noreply.github.com>
2026-02-21 22:38:13 +0530

    Time: 0 ms (100%), Space: 18.5 MB (26.54%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -1,30 +1,31 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    bool isMirror(TreeNode* left,TreeNode* right){
-        if(left==NULL && right==NULL)   return true;
 
-        if(left==NULL || right==NULL)return false;
-
-        if(left->val!=right->val)return false;
-
-        bool outer=isMirror(left->left , right->right);
-        bool inner=isMirror(left->right, right->left);
-        return outer && inner;
-    }
     bool isSymmetric(TreeNode* root) {
-        if(root==NULL)return true;
+        if (root == NULL)
+            return true;
 
-        return isMirror(root->left,root->right);
+        return isMirror(root->left, root->right);
+    }
+
+    bool isMirror(TreeNode* left, TreeNode* right) {
+
+        // 1️⃣ Agar dono NULL hain
+        if (left == NULL && right == NULL)
+            return true;
+
+        // 2️⃣ Agar ek NULL hai
+        if (left == NULL || right == NULL)
+            return false;
+
+        // 3️⃣ Agar values different hain
+        if (left->val != right->val)
+            return false;
+
+        // 4️⃣ Mirror direction me check karo
+        bool outer = isMirror(left->left, right->right);
+        bool inner = isMirror(left->right, right->left);
+
+        return outer && inner;
     }
 };
