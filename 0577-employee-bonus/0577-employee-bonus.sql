d559f643eabbd04fd4715d92f48c3cad6180f314
Rishabh <149367535+rishabh-ydv23@users.noreply.github.com>
2026-05-07 22:57:00 +0530

    Time: 1218 ms (10.12%), Space: 0B (100%) - LeetHub

--- 0577-employee-bonus/0577-employee-bonus.sql
+++ 0577-employee-bonus/0577-employee-bonus.sql
@@ -1,5 +1,13 @@
-SELECT e.name, b.bonus
-FROM Employee e
+Select e.name, b.bonus from Employee e
 LEFT JOIN Bonus b
-ON e.empId = b.empId
-WHERE b.bonus < 1000 OR b.bonus IS NULL;
+ON e.empId =b.empId
+where b.bonus < 1000 OR b.bonus iS NULL
+
+
+-- SQL me IS specially NULL check karne ke liye use hota hai.
+
+-- NULL means:
+-- value missing hai
+-- ya value exist nahi karti
+
+-- SQL me NULL ko = se compare nahi kar sakte..
