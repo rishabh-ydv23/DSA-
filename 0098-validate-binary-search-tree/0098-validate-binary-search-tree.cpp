18c9ff4fe75a1a82cf751719d743aee72e67221b
Rishabh <149367535+rishabh-ydv23@users.noreply.github.com>
2026-05-09 23:09:28 +0530

    Time: 0 ms (100%), Space: 21.7 MB (94.69%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -0,0 +1,21 @@
+/*
+
+Each node must lie within (min, max)
+Left child → (min, node.val)
+Right child → (node.val, max)
+
+*/
+class Solution {
+public:
+    bool solve(TreeNode* root, long long low,long long high){
+        if(root==NULL)return true;
+
+        if(root->val <=low || root->val >=high) return false;
+
+        return solve(root->left , low, root->val) && solve(root->right, root->val,high);
+    }
+
+    bool isValidBST(TreeNode* root) {
+        return solve(root,LLONG_MIN, LLONG_MAX);
+    }
+};
