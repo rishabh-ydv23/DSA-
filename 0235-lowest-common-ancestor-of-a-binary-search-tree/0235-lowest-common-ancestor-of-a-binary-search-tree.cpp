e6554dad2e698a76f4423a226591049abed3e64d
Rishabh <149367535+rishabh-ydv23@users.noreply.github.com>
2026-05-10 12:23:41 +0530

    Time: 15 ms (97.44%), Space: 23.4 MB (90.89%) - LeetHub

---
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -0,0 +1,31 @@
+/**
+
+“Lowest” means closest to the given nodes, not the root.
+The LCA is the first node where paths to both nodes meet.
+
+Because it is a BST, we can use this property:
+Left subtree values < root
+Right subtree values > root
+
+For nodes p and q:
+If both are smaller than root → go left
+If both are greater than root → go right
+Otherwise → current root is the LCA
+
+ */
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        while(root!=NULL){
+            if(p->val < root->val && q->val < root->val){
+                root=root->left;
+            }else if(p->val > root->val && q->val > root->val){
+                root=root->right;
+            }else{
+                return root;
+            }
+        }
+        return NULL;
+    }
+};
